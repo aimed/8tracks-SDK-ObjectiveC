84f821275bad2a132c59b77c4f71ccc557560b18
Maximilian Täschner <maximilian.taeschner@gmail.com>
2014-06-15 08:47:54 +0200

    Initial Commit

---
+++ EightTracksSDK/EightTracksSDK.h
@@ -0,0 +1,13 @@
+//
+//  EightTracksSDK.h
+//  EightTracksSDK
+//
+//  Created by Maximilian Täschner on 15/06/14.
+//  Copyright (c) 2014 Norocketlab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EightTracksSDK : NSObject
+
+@end
---
+++ EightTracksSDK/EightTracksSDK.m
@@ -0,0 +1,13 @@
+//
+//  EightTracksSDK.m
+//  EightTracksSDK
+//
+//  Created by Maximilian Täschner on 15/06/14.
+//  Copyright (c) 2014 Norocketlab. All rights reserved.
+//
+
+#import "EightTracksSDK.h"
+
+@implementation EightTracksSDK
+
+@end
